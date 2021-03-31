b0426926a13322327a1b37b8c513d12bb448a252
JakubNajman <najman00@outlook.com>
2021-03-31 21:00:34 +0200

    Podpowiedzie

--- src/com/main/Exposure.java
+++ src/com/main/Exposure.java
@@ -3,7 +3,7 @@ package com.main;
 public class Exposure {
     // Data.
     private int contactDays;
-   private Contact[] contacts;
+    private Contact[] contacts; //nazwa nie typ
 
     // Constructor.
     public Exposure(int contactDays){
@@ -12,8 +12,15 @@ public class Exposure {
         // TODO: Make dynamic array which length is equal to contactDays.
     }
 public String getSummary() {
-        return "test";
+        for(int i = 1; i<=contactDays; i++){
+            String summary = contacts[i].getSummary();
+            // [Typ: NO (0.0)]
+            return "coś"+i+summary;
+            // coś2[Typ: NO (0.0)]
+        }
+    return null;
 }
+
 public int getContactDays () {
         return contactDays;
 }
