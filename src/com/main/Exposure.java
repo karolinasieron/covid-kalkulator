8791d2fb5e4000da5e4125e32c82db966f41c906
karolinasieron <karolinasieron@icloud.com>
2021-04-01 19:47:30 +0200

    kolejne

--- src/com/main/Exposure.java
+++ src/com/main/Exposure.java
@@ -12,11 +12,21 @@ public class Exposure {
         // TODO: Make dynamic array which length is equal to contactDays.
     }
 public String getSummary() {
+        System.out.println ("Exposure: [Time horizon:" +this.contactDays+ "day(s)]")
+        for (int i = 1; i< contactDays; i++) }
+            Exposure.getContactDays();
+            String summary = contacts[i].getSummary();
+            return "Day" +contacts [i]+ ": [Typ:" + summary; + "]"
+            return null;
+
+
+
         return "test";
 }
 public int getContactDays () {
         return contactDays;
 }
+
 
     public Contact[] getContacts() {
         return contacts;
